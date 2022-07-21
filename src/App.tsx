097f7e7e44f52dc5f51c094e30d66a6fe0a2d064
Brittany Steinkirchner <bsteinkirchner@outlook.com>
2022-07-21 15:26:54 -0500

    navdesign

--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,23 @@ import { Home } from "./pages/Home";
 import { Searched } from "./pages/Searched"
 import { Store } from "./pages/Store";
 import { Checkout } from "./pages/Checkout";
+import { About } from "./pages/About";
+import { Navbar } from "./components/Navbar";
 
 function App() {
   return (
-    <Container>
+    <>
+    <Navbar />
+    <Container className="mb-4">
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/searched" element={<Searched />} />
         <Route path="/store" element={<Store />} />
+        <Route path="/about" element={<About />} />
         <Route path="/checkout" element={<Checkout />} />
       </Routes>
     </Container>
+    </>
   );
 }
 
