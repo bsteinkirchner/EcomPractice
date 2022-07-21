d487d2c3300ac79e399ef4eabb7d8fba62df610f
Brittany Steinkirchner <bsteinkirchner@outlook.com>
2022-07-21 15:41:38 -0500

    store shell

---
+++ src/components/StoreItem.tsx
@@ -0,0 +1,48 @@
+import { Card, Button } from "react-bootstrap";
+
+type StoreItemProps = {
+  id: number,
+  brand: string,
+  name: string,
+  price: number
+  imgUrl: string,
+  available: boolean
+}
+
+export function StoreItem({ id, brand, name, price, imgUrl, available } : StoreItemProps) {
+    const quantity = 0;
+    return (
+    <Card className="h-100">
+            <Card.Img
+                variant="top"
+                src={imgUrl}
+                height="200px"
+                style={{ objectFit: "cover" }}
+            />
+            <Card.Body className="d-flex flex-column">
+                <Card.Title className="d-flex justify-content-between align-items-baseline mb-4">
+                    <span className="fs-2">{name}</span>
+                    <span className="ms-2 text-muted">{price}</span>
+                </Card.Title>
+                <div className="mt-auto">
+                    {quantity === 0 ? (
+                        <Button className="w-100">+ Add to Cart</Button>
+                    ) : <div className="d-flex align-items-center flex-column"
+                                style={{gap: "0.5rem"}}>
+                            <div className="d-flex align-items-center justify-content-center"
+                                    style={{gap: "0.5rem"}}>
+                                <Button>-</Button>
+                                <div>
+                                    <span className="fs-3">{quantity}</span> in cart
+                                </div>
+                                <Button>+</Button>
+                            </div>
+                            <Button variant="danger">Remove</Button>
+                        </div>
+                    }
+                </div>
+            </Card.Body>
+        </Card>
+  )
+}
+
